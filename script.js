7a5d389fe1bca32896a4ca3eac3ff82a7964e06e
Victor Souza <victorsouzavieira.22@gmail.com>
2025-05-02 23:35:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const container = document.createElement('div') 
+container.className = "container"
+let styleQuestions = document.createElement('div')
+styleQuestions.className = "styleQuestions"
+let question = document.createElement('h2') 
+question = document.className = "question"
+
+container.appendChild(styleQuestions)
