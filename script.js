bd69ae9b9ede93ed5ad3209eb688afb1c15872da
Victor Souza <victorsouzavieira.22@gmail.com>
2025-05-03 23:53:23 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-
+window.onload = function minhaPagina() {
 const container = document.createElement('div') 
 container.className = "container"
 let styleQuestions = document.createElement('div')
 styleQuestions.className = "styleQuestions"
-let question = document.createElement('h2') 
-question = document.className = "question"
+let divH2Quiz = document.createElement('div')
+divH2Quiz.className = "divH2"
+const quizH2 = document.createElement('h2');
+quizH2.innerHTML = "Quiz Javascript";
+quizH2.className = "quizH2";
 
+document.body.appendChild(divH2Quiz)
+document.body.appendChild(container);
 container.appendChild(styleQuestions)
+divH2Quiz.appendChild(quizH2)
+}
+
