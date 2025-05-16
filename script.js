3054bf02d1331a800cd0d1456f8d007005de3ccf
Victor Souza <victorsouzavieira.22@gmail.com>
2025-05-16 17:08:31 -0300

    fazendo as perguntas e a resposta aparecem na tela

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ startQuizButton.innerHTML = "Start Quiz"
 let nextButton = document.createElement('button')
 nextButton.className = "nextButton"
 nextButton.innerHTML = "Next"
-nextButton.style.display = "none"
 let divNextBtn = document.createElement('div')
 divNextBtn.className = "divNextBtn"
+divNextBtn.style.display = "none"
+
 let QuestionPosition = 0
 
 document.body.appendChild(divH2Quiz)
@@ -25,15 +26,27 @@ document.body.appendChild(container);
 divH2Quiz.appendChild(quizH2)
 divNextBtn.appendChild(nextButton)
 container.appendChild(divNextBtn)
-container.appendChild(startQuizButton)
+styleContainerQuestions.appendChild(startQuizButton)
+container.appendChild(styleContainerQuestions)
 
-startQuizButton.addEventListener('click', function() {
-    renderQuiz(questionsAndAnswers, container);
-    nextButton.style.display = "flex"
-    this.remove();
+function renderQuiz(position, container) {
+    container.innerHTML = '';
+    const questionElement = document.createElement('h2');
+    questionElement.textContent = questionsAndAnswers[position].question;
+    container.appendChild(questionElement);
+    questionElement.className ="questionH2"
+}
+    startQuizButton.addEventListener('click', function() {
+    renderQuiz(QuestionPosition, styleContainerQuestions);
+    divNextBtn.style.display = "flex";
+
+    nextButton.addEventListener('click', function() {
+    QuestionPosition = (QuestionPosition + 1)  
+    renderQuiz(QuestionPosition, styleContainerQuestions);
+    });
+
 });
 
-}
 
 const questionsAndAnswers = [
 {
@@ -42,31 +55,16 @@ altenativa: ["Corinthians", "Sport Club Corinthians"],
 AnswersCorrect: 1
 },
 
-// {
-// question: "melhor   time da série b",
-// altenativa: ["Vasco da gama", "Vasco"],
-// AnswersCorrect: 0
-// },
-// {
-// question: "A or B",
-// altenativa: ["A", "B"],
-// AnswersCorrect: 0
-// }
+{
+ question: "melhor   time da série b",
+ altenativa: ["Vasco da gama", "Vasco"],
+ AnswersCorrect: 0
+ },
+ {
+ question: "A or B",
+ altenativa: ["A", "B"],
+ AnswersCorrect: 0
+ }
 ]
 
-// startButton.addEventListener('click', function() {
-//     this.remove();
-//     nextButton.style.display = "block";
-//     showQuestion();
-// });
-
-function renderQuiz(questions, container) {
-    questions.forEach(item => {
-    let questionH2 = document.createElement('h2');
-    questionH2.textContent = item.question;
-    container.appendChild(questionH2);
-} )
 }
-
-// passar para próxima pergunta e tirar a anterior
-
